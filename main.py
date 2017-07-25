d8d7f21ae258ef44403adc12d03bb7ea7d081ba8
Sardor Muminov <sardor@insilicogen.com>
2017-07-25 12:23:33 +0900

    Add logger instance to bot

--- main.py
+++ main.py
@@ -12,13 +12,18 @@ from bot import bot
 
 # Misc
 import os
+import sys
+import logging
 
 # Use uvloop
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 async def run_bot():
-    await bot.loop()
+    try:
+        await bot.loop()
+    except:
+        bot.logger.error('Cannot run bot. Check environment variables and try again.')
 
 
 async def make_pg_pool():
@@ -31,9 +36,15 @@ async def make_pg_pool():
 # Main event loop
 loop = asyncio.get_event_loop()
 
-# Attach postgres connection pool to bot
+# Attach logger instance to bot
+logger = logging.getLogger('bot')
+logging.basicConfig(level=logging.DEBUG)
+setattr(bot, 'logger', logging)
+
+# Attach postgres connection pool instance to bot
 pg_pool = loop.run_until_complete(make_pg_pool())
 setattr(bot, 'pg_pool', pg_pool)
+
 
 
 if __name__ == '__main__':
