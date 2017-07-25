a5e71bc10fe0d3916cc87f8a62477686d7a6b598
Sardor Muminov <sardor@insilicogen.com>
2017-07-25 12:17:03 +0900

    Add db connection instance

--- main.py
+++ main.py
@@ -20,11 +20,20 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 async def run_bot():
     await bot.loop()
 
+
+async def make_pg_pool():
+    dsn = os.environ.get('DATABASE_URL')
+    return await create_pg_pool(
+        dsn=dsn,
+        min_size=5,
+        max_size=10)
+
 # Main event loop
 loop = asyncio.get_event_loop()
 
-# Attach admin group to bot
-setattr(bot, 'group', bot.private(os.environ.get('BOT_GROUP')))
+# Attach postgres connection pool to bot
+pg_pool = loop.run_until_complete(make_pg_pool())
+setattr(bot, 'pg_pool', pg_pool)
 
 
 if __name__ == '__main__':
